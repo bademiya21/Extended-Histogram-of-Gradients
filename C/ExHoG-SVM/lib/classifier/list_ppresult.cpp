82e3f1f069ab45b795d0aa6194d847ffcc2b31d1
Amit Satpathy <bademiya21@users.noreply.github.com>
2017-12-06 16:27:59 +0800

    Add files via upload

---
+++ C/ExHoG-SVM/lib/classifier/list_ppresult.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <lear/exception.h>
+
+#include <lear/classifier/list_ppresult.h>
+
+lear::List_PPResult::List_PPResult(
+        const std::string& listfile_,
+        const int marker,
+		const bool hardtest)
+            :
+    Parent(),
+    to(listfile_.c_str()), to1(listfile_), marker(marker), hardtest(hardtest)
+{
+    using namespace std;
+    if (hardtest){
+		if (!to) {
+			throw lear::Exception("List_PPResult::constructor()",
+                    "Unable to open output file " + listfile_);
+		}
+	}
+}
+
+void lear::List_PPResult::write( 
+    const_iterator s, const_iterator e, const std::string filename)
+{// {{{
+     using namespace std;
+	 if (!hardtest){
+		 string file_list = filename;
+		 string str = "txt";
+		 string::iterator it = file_list.end()-3;
+		 file_list.erase(it,file_list.end());
+		 file_list.append(str);
+			 
+		 it = file_list.end() - 22;
+		 file_list.erase(file_list.begin(),it);
+		 string file_list1 = to1;
+		 file_list1.append(file_list);
+			 
+		 ofstream file(file_list1.c_str());	
+		 for (; s != e; ++s) 
+			file  << *s << endl;
+	 }
+	 else{
+		to  << setw(6) << 0 << ' ' << setw(6) << 0 << ' ' 
+			<< setw(6) << 0 << ' ' << setw(6) << 0 << ' ' 
+			<< setw(6) << 0 << ' ' << setw(6) << 0 << ' '
+			<< setw(6) << 0 << '\n';
+		for (; s != e; ++s) 
+			to  << *s << setw(4) << marker << '\n';
+	 }
+
+}// }}}
+
---
+++ C/ExHoG-SVM/lib/imageutil.cpp
@@ -0,0 +1,54 @@
+/*
+ * =====================================================================================
+ * 
+ *        Filename:  imageutil.cpp
+ * 
+ *     Description:  Utilitiy functions for images.
+ * 
+ *         Version:  1.0
+ *         Created:  06/11/2006 09:46:03 AM IST
+ *        Revision:  none
+ *        Compiler:  gcc
+ * 
+ *          Author:  Navneet DALAL
+ *         Company:  Lear./INRIA
+ * 
+ * =====================================================================================
+ */
+
+/* standard headers */
+#include <X11/Xlib.h>
+#include <Imlib2.h>
+#include <lear/exception.h>
+#include <lear/image/imageutil.h>
+
+void lear::imagesize(const std::string& imagename,int& width, int& height) 
+{
+    Imlib_Image image = imlib_load_image(imagename.c_str());;
+    
+    if (image)// if the load was successful
+    {   // set the image we loaded as the current context image to work on
+        imlib_context_set_image(image);
+        width = imlib_image_get_width(), 
+        height = imlib_image_get_height();
+        imlib_free_image();
+    } else {
+        throw Exception("ImageOp::read", 
+                "Unable to read image: " + imagename);
+    }
+}
+
+std::string lear::imagefmt(const std::string& imagename) {
+    Imlib_Image image = imlib_load_image(imagename.c_str());
+    
+    if (image)// if the load was successful
+    {   // set the image we loaded as the current context image to work on
+        imlib_context_set_image(image);
+        
+        std::string format (imlib_image_format());
+        imlib_free_image();
+        return format;
+    } else {
+        return std::string();
+    }
+}
